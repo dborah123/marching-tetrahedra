ccfc59fcf80a627b1a7c5943338a5cd1e2b27095
dborah123 <dborah@middlebury.edu>
2022-06-19 21:08:51 +0000

    organizing header file a little

--- marchingtet.h
+++ marchingtet.h
@@ -14,8 +14,6 @@ public:
 MarchingTet(Grid<Tet>& tet_grid, TetFunction& function);
 void marching_tets();
 
-void reconstruct_tet_surface(int tet_index);
-
 /* Utility Functions */
 Mesh<Triangle> get_mesh();
 HalfEdgeMesh<Triangle> get_halfmesh();
@@ -26,24 +24,29 @@ Grid<Tet>& _tet_grid;
 TetFunction& _function;
 std::map<std::set<int>, double> _inserted_edges; // Maps two vertices(edge) to 
                                                  // intersection 
-std::map<int,double> _isovalues;                 // Maps vertex index to isovalue
+std::map<int,double> _isovalues;                 // Maps tet vertex index to isovalue
 Mesh<Triangle> _mesh;
 
-/* Tables */
+/* Static Tables */
 std::map<int,int> _edgetable;
 std::map<int, std::vector<int>> _triangletable;
 
 /* Initialization Functions */
 void initialize_edgetable();
 void initialize_triangletable();
-
 void preprocess_isovalues();
+
+void reconstruct_tet_surface(int tet_index);
+
 vec3d get_intersection_point(int v0_index, int v1_index);
 int determine_case(std::vector<double>& tet_isovalues);
 void get_tet_isovalues(int tet_index, std::vector<double>& tet_isovalues);
+
+/* Adding Elements to Mesh */
 void create_triangles(int tet_index, std::vector<int>& triangles_to_create);
 int add_vertex_to_mesh(int tet_index, int index0, int index1);
 
+/* Orientating New Triangle */
 int check_orientation(int *tri_indices);
 void change_orientation(int *tri_indices);
 
